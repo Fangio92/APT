fd07de7bd1ae44b99c2ddce2e288b3bee19d6de0
Damir Dizdarevic <dizdarevic@zoho.com>
2021-05-16 21:24:11 +0200

    Toast error

--- app/src/main/java/com/dizdarevic/apt/ui/fragments/UsersFragment.kt
+++ app/src/main/java/com/dizdarevic/apt/ui/fragments/UsersFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -70,7 +71,7 @@ class UsersFragment : Fragment() {
         })
 
         viewModel.errorMessage.observe(viewLifecycleOwner, Observer {
-
+            Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
         })
     }
 
